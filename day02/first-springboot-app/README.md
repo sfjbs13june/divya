a8dc76cdb398b4acc12dbb04642059a73a1197dc
divya <battula.divya@tcs.com>
2023-01-13 06:44:59 +0000

    second-springboot-app

---
+++ day02/first-springboot-app/README.md
@@ -0,0 +1,12 @@
+# create first-springboot-app
+```
+https://start.spring.io/
+```
+## build the app
+```
+mvn clean install
+```
+## run the application
+```
+mvn spring-boot:run
+```
---
+++ day02/second-springboot-app/src/main/java/com/divya/demo/SecondSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.divya.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringbootDemoApplication.class, args);
+	}
+
+}
