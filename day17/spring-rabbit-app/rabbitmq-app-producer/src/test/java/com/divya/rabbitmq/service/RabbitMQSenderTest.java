724f002d843b82e4f8c975ff64ca0ec5551b5367
divya <battula.divya@tcs.com>
2023-02-07 11:21:01 +0000

    rabbitmq assesment

---
+++ day17/spring-rabbit-app/rabbitmq-app-producer/src/test/java/com/divya/rabbitmq/service/RabbitMQSenderTest.java
@@ -0,0 +1,34 @@
+package com.divya.rabbitmq.service;
+
+import com.divya.rabbitmq.model.Employee;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.amqp.core.AmqpTemplate;
+
+import org.springframework.test.context.ActiveProfiles;
+import static org.hamcrest.Matchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles(value = "test")
+public class RabbitMQSenderTest {
+
+  @InjectMocks
+  RabbitMQSender rabbitMQSender;
+
+  @Mock
+  AmqpTemplate amqpTemplate;
+
+@Test
+  public void sendTest() {
+    Employee employee=new Employee();
+   // doNothing().when(amqpTemplate).convertAndSend(anyString(),anyString(),any(Employee.class));
+    rabbitMQSender.send(employee);
+   //verify(amqpTemplate, times(1)).convertAndSend("exchange","queue",employee);
+  }
+
+
+}
